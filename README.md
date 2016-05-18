710d3aaa445da2d893273b798ee7fab092946142
Aureliano <sersinfin@gmail.com>
2016-05-18 09:10:25 -0700

    Simple ellipse.

---
+++ README.md
@@ -0,0 +1,2 @@
+### Information on project
+
---
+++ sketch.js
@@ -0,0 +1,14 @@
+/**
+ * Created by nani on 5/14/16.
+ */
+
+// A starting point
+
+function setup() {
+
+}
+
+function draw() {
+    ellipse(50, 50, 50, 50);
+}
+
